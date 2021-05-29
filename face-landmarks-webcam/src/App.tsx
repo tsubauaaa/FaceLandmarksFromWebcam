6c3955cef0c70d8e197c5540b0eeb7ab735bb894
ts-hirota <ts-hirota@kddi.com>
2021-05-29 23:04:15 +0900

    顔の特徴点を取得した

--- face-landmarks-webcam/src/App.tsx
+++ face-landmarks-webcam/src/App.tsx
@@ -1,18 +1,55 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import "./App.css";
 import Webcam from "react-webcam";
+import "@tensorflow/tfjs-core";
+import "@tensorflow/tfjs-converter";
+import "@tensorflow/tfjs-backend-webgl";
+import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+import { MediaPipeFaceMesh } from "@tensorflow-models/face-landmarks-detection/dist/types";
 
-function App() {
-  const webcam = useRef<Webcam>(null);
+
+
+const App: React.FC = () => {
+  // Setup references
+  const webcamRef = useRef<Webcam>(null);
+
+  // Load Facemesh
+  const runFacemesh = async () => {
+    const net = await faceLandmarksDetection.load(
+      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
+    );
+      setInterval(() => {
+        detectFaceLandmarks(net);
+      }, 100);
+
+  };
+
+  const detectFaceLandmarks = async (net: MediaPipeFaceMesh) => {
+    // readyState = 4 is HAVE_ENOUGH_DATA
+    if(typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
+      const video = webcamRef.current.video;
+      const face = await net.estimateFaces({
+        input: video,
+      });
+      if(face.length > 0) {
+        console.log(face);
+      }
+    } else {
+      return;
+    }
+  };
+
+  runFacemesh();
 
   return (
     <div className="App">
-      <header className="header">
+      <header className="App-header">
         <div className="title">Face Landmarks Detection</div>
-        <Webcam audio={false} ref={webcam} height={1080} width={1280} />
+        <Webcam audio={false} ref={webcamRef} height={480} width={640} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
+
